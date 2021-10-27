5adcdf0458abb71ca44f93d46047a08729d9176e
Keith Tom <keith.tom@gmail.com>
2021-10-27 12:51:41 -0700

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :people, only: %i[index show edit update]
+  # create (on signup, read, update (if you), destroy? never.)
+
+  resources :teacher_leaders, only: %i[index show edit update]
+  resources :partners, only: %i[index show edit update]
+
+  resources :schools, only: %i[index show edit update]
 end
