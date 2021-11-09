32b51cc96cee22055cfad080986ee638dc657ded
Keith Tom <keith.tom@gmail.com>
2021-11-09 15:00:17 -0800

    Stub two user commands as concept

---
+++ app/commands/users/offboard.rb
@@ -0,0 +1,12 @@
+module Commands
+  module Users
+    class Offboard
+      # inverse of onboard.
+      # user becomes inactive or leaves network
+      # disable google account
+      # remove them from google groups
+      # suspend profile
+      # suspend school profile
+    end
+  end
+end
