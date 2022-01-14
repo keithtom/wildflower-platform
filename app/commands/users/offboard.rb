bbfcc395dde07c7a55a1e45e99415d04972614ba
Keith Tom <keith.tom@gmail.com>
2022-01-13 17:53:14 -0800

    fix error

--- app/commands/users/offboard.rb
+++ app/commands/users/offboard.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-module Commands
-  module Users
-    class Offboard
-      # inverse of onboard.
-      # user becomes inactive or leaves network
-      # disable google account
-      # remove them from google groups
-      # suspend profile
-      # suspend school profile
-    end
+module Users
+  class Offboard
+    # inverse of onboard.
+    # user becomes inactive or leaves network
+    # disable google account
+    # remove them from google groups
+    # suspend profile
+    # suspend school profile
   end
 end
