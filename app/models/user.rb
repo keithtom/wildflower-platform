3f74f9cdaae478e8d6fea8c0a8cdc10b99d15d61
Keith Tom <keith.tom@gmail.com>
2022-01-31 13:47:56 -0800

    minor updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   belongs_to :person, :optional => true
+
+   def password_required?
+     false
+   end
+
+   def name
+     "#{first_name} #{last_name}".strip if first_name
+   end
 end
--- spec/factories/person/experiences.rb
+++ spec/factories/person/experiences.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
     name { Faker::Lorem.word.titleize }
     description { Faker::Lorem.paragraphs }
     start_date { Faker::Date.between(from: 5.years.ago, to: 3.months.ago) }
-    end_date { }
+    end_date { Faker::Date.between(from: 3.months.ago, to: 1.month.ago)  }
   end
 end
