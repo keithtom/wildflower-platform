48629fc583032557176d07668f9495511c351f32
Keith Tom <keith.tom@gmail.com>
2022-01-31 14:24:22 -0800

    School: Serializer more things

--- app/serializers/v1/school_serializer.rb
+++ app/serializers/v1/school_serializer.rb
@@ -2,7 +2,8 @@
 
 module V1
   class SchoolSerializer < ApplicationSerializer
-    attributes :name
+    attributes :name, :short_name, :website, :phone, :email, :governance_type, :tuition_assistance_type, :ages_served, :calendar,
+               :max_enrollment, :facebook, :instagram
 
     has_one :address
   end
