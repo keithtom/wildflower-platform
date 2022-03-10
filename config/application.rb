a2eb268a096a9582018108c0081d346c74bbc184
Keith Tom <keith.tom@gmail.com>
2022-03-10 11:27:36 -0800

    Fix production error

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ Bundler.require(*Rails.groups)
 
 module WildflowerPlatform
   class Application < Rails::Application
-    config.api_only = true
-    
+    config.api_only = false
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
