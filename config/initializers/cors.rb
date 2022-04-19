0ca0119b95c4c588dca9ec083bfe744f96b51379
Keith Tom <keith.tom@gmail.com>
2022-04-19 08:51:49 -0700

    Try this

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,13 +4,14 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-
+# Access-Control-Allow-Origin
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", /\Ahttps:\/\/.+\.vercel\.app/\z, "https://platform.wildflowerschools.org", "https://journey.wildflowerschools.org", "https://staging-journey.wildflowerschools.org"
-
+    # origins "localhost:3000", /\Ahttps:\/\/.+\.vercel\.app/\z, "https://platform.wildflowerschools.org", "https://journey.wildflowerschools.org", "https://staging-journey.wildflowerschools.org"
+    origins '*'
     resource "*",
       headers: :any,
+      # credentials: true,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
