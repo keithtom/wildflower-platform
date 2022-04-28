1f7d39c48aa542fc1f496cb30cec966d0dc464e5
Keith Tom <keith.tom@gmail.com>
2022-04-28 15:52:09 -0700

    Make pod contacts optional

--- app/models/pod.rb
+++ app/models/pod.rb
@@ -2,5 +2,5 @@ class Pod < ApplicationRecord
   include ApplicationRecord::ExternalIdentifier
 
   belongs_to :hub
-  belongs_to :primary_contact, class_name: 'Person'
+  belongs_to :primary_contact, class_name: 'Person', optional: true
 end
