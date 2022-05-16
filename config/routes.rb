59a7c1aab7ad9aae31036868993b990db74d75b7
Keith Tom <keith.tom@gmail.com>
2022-05-16 11:10:55 -0700

    Advice: Stub API routes
            v1_advice_person_decisions_draft GET    /v1/advice/people/:person_id/decisions/draft(.:format)                                            v1/advice/decisions#draft
             v1_advice_person_decisions_open GET    /v1/advice/people/:person_id/decisions/open(.:format)                                             v1/advice/decisions#open
           v1_advice_person_decisions_closed GET    /v1/advice/people/:person_id/decisions/closed(.:format)                                           v1/advice/decisions#closed
                  v1_advice_person_decisions GET    /v1/advice/people/:person_id/decisions(.:format)                                                  v1/advice/decisions#index
                     open_v1_advice_decision PUT    /v1/advice/decisions/:id/open(.:format)                                                           v1/advice/decisions#open
                    close_v1_advice_decision PUT    /v1/advice/decisions/:id/close(.:format)                                                          v1/advice/decisions#close
                          v1_advice_messages GET    /v1/advice/decisions/:id/messages(.:format)                                                       v1/advice/messages#index
                                             POST   /v1/advice/decisions/:id/messages(.:format)                                                       v1/advice/messages#create
                           v1_advice_message PATCH  /v1/advice/decisions/:id/messages/:id(.:format)                                                   v1/advice/messages#update
                                             PUT    /v1/advice/decisions/:id/messages/:id(.:format)                                                   v1/advice/messages#update
                                             DELETE /v1/advice/decisions/:id/messages/:id(.:format)                                                   v1/advice/messages#destroy
                      v1_advice_stakeholders GET    /v1/advice/decisions/:id/stakeholders(.:format)                                                   v1/advice/stakeholders#index
                                             POST   /v1/advice/decisions/:id/stakeholders(.:format)                                                   v1/advice/stakeholders#create
                       v1_advice_stakeholder DELETE /v1/advice/decisions/:id/stakeholders/:id(.:format)                                               v1/advice/stakeholders#destroy
                         v1_advice_decisions POST   /v1/advice/decisions(.:format)                                                                    v1/advice/decisions#create
                          v1_advice_decision GET    /v1/advice/decisions/:id(.:format)                                                                v1/advice/decisions#show
                                             PATCH  /v1/advice/decisions/:id(.:format)                                                                v1/advice/decisions#update
                                             PUT    /v1/advice/decisions/:id(.:format)                                                                v1/advice/decisions#update
                                                            v1/advice/decisions#update

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,26 @@ Rails.application.routes.draw do
       # resources :school_relationships
     end
 
+    namespace :advice do
+      resources :people, :only => [] do
+        get "decisions/draft"
+        get "decisions/open"
+        get "decisions/closed"
+        resources :decisions, only: [:index] # as an API, the state would be a query parameter, if we want to be restful.
+      end
+
+      resources :decisions, only: [:show, :create, :update] do
+        member do
+          put :open
+          put :close
+
+          resources :messages, only: [:index, :create, :update, :destroy]
+          resources :stakeholders, only: [:index, :create, :destroy]
+        end
+      end
+
+    end
+
     # resources :hubs, except: :destroy do
     #   resources :pods, except: :destroy
     # end
