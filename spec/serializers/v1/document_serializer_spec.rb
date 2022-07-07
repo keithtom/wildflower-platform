9930b40cd63baf1f2c85e75112579c7d03eb3bab
Keith Tom <keith.tom@gmail.com>
2022-07-07 12:10:26 -0700

    Fix document serializer spec

--- spec/serializers/v1/document_serializer_spec.rb
+++ spec/serializers/v1/document_serializer_spec.rb
@@ -6,9 +6,8 @@ describe V1::DocumentSerializer do
   subject { described_class.new(document).as_json }
 
   it "should serialize properly" do
-    pp subject
     expect(json_document['data']).to have_id("doc123")
     expect(json_document['data']).to have_type("document")
-    expect(json_document['data']).to have_jsonapi_attributes(:inheritance_type, :title, :link)
+    expect(json_document['data']).to have_jsonapi_attributes(:inheritanceType, :title, :link)
   end
 end
