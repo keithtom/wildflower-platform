7655d3a0884daf1cc3f019ad82af19ffb46da4a4
Keith Tom <keith.tom@gmail.com>
2022-10-04 09:56:25 -0700

    Stub instance folder and account idea

---
+++ gems/workflow/app/models/workflow/instance/workflow.rb
@@ -0,0 +1,13 @@
+# definition, version, it contains the state of completed steps.
+# so you know all the processes (they have state? cached?)
+# so has all the steps
+  - assignee
+  - started_at
+  - completed_at
+  - role
+  - see commands...
+  - has manual steps
+  - also has a user id, every user has their own instance of the definition with state
+    - so there's a external_data_state
+    - ask maggie: users can never change the dependencies? just add new ones for manual
+    -
