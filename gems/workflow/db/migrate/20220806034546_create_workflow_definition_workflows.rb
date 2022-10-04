9e7831231dae057043b30de9dece9855c9365776
Keith Tom <keith.tom@gmail.com>
2022-10-04 09:55:50 -0700

    Adding some comments and misc cleanup

--- gems/workflow/db/migrate/20220806034546_create_workflow_definition_workflows.rb
+++ gems/workflow/db/migrate/20220806034546_create_workflow_definition_workflows.rb
@@ -1,8 +1,8 @@
 class CreateWorkflowDefinitionWorkflows < ActiveRecord::Migration[7.0]
   def change
     create_table :workflow_definition_workflows do |t|
-      t.string :name
       t.string :version
+      t.string :name
       t.text :description
 
       # author?
--- gems/workflow/spec/models/workflow/definition/workflow_spec.rb
+++ gems/workflow/spec/models/workflow/definition/workflow_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 module Workflow
   RSpec.describe Definition::Workflow, type: :model do
     pending "add some examples to (or delete) #{__FILE__}"
+
+    # create a workflow from lib of processes and add dependencies.
   end
 end
