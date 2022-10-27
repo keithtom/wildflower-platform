16ba2179846211132c857370d38074828b72bd94
Li Ouyang <li.ouyang@gmail.com>
2022-10-27 09:12:45 -0400

    use external id

--- app/serializers/v1/workflow/process_serializer.rb
+++ app/serializers/v1/workflow/process_serializer.rb
@@ -1,8 +1,13 @@
 class V1::Workflow::ProcessSerializer < ApplicationSerializer
   attributes :title, :effort, :categories, :status, :position #, :assignee
 
-  belongs_to :workflow, record_type: :workflow_instance_workflow, id_method_name: :workflow_instance_workflow_id,
-    serializer: V1::Workflow::WorkflowSerializer
+  belongs_to :workflow, record_type: :workflow_instance_workflow, id_method_name: :external_identifier,
+    serializer: V1::Workflow::WorkflowSerializer do |process|
+      process.workflow
+    end
 
-  has_many :steps, serializer: V1::Workflow::StepSerializer, record_type: :workflow_instance_step
+  has_many :steps, serializer: V1::Workflow::StepSerializer, record_type: :workflow_instance_step,
+    id_method_name: :external_identifier do |process|
+      process.steps
+    end
 end
--- app/serializers/v1/workflow/workflow_serializer.rb
+++ app/serializers/v1/workflow/workflow_serializer.rb
@@ -1,7 +1,10 @@
 class V1::Workflow::WorkflowSerializer < ApplicationSerializer
   attributes :name, :description, :version
 
-  has_many :processes, serializer: V1::Workflow::ProcessSerializer, record_type: :workflow_instance_process
+  has_many :processes, serializer: V1::Workflow::ProcessSerializer, record_type: :workflow_instance_process, 
+    id_method_name: :external_identifier do |workflow|
+      workflow.processes
+  end
 
   link :url
 end
