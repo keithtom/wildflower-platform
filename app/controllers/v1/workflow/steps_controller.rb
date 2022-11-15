2d6c70ec3537c315b1db340e433bda775478f842
Li Ouyang <li.ouyang@gmail.com>
2022-11-15 11:33:31 -0500

    wip

--- app/controllers/v1/workflow/steps_controller.rb
+++ app/controllers/v1/workflow/steps_controller.rb
@@ -1,8 +1,21 @@
 class V1::Workflow::StepsController < ApiController
   def show
     # TODO: identify current user, check if process/step id is accessible to user
-    @process = Workflow::Instance::Process.find_by!(external_identifier: params[:process_id])
-    @step = @process.steps.find_by!(external_identifier: params[:id])
-    render json: V1::Workflow::StepSerializer.new(@step)
+    process = Workflow::Instance::Process.find_by!(external_identifier: params[:process_id])
+    step = process.steps.find_by!(external_identifier: params[:id])
+    render json: V1::Workflow::StepSerializer.new(step)
+  end
+
+  def update
+    process = Workflow::Instance::Process.find_by!(external_identifier: params[:process_id])
+    step = process.steps.find_by!(external_identifier: params[:id])
+    step.update!(step_params)
+    render json: V1::Workflow::StepSerializer.new(step)
+  end
+
+  private
+
+  def step_params
+    params.require(:step).permit(:title, :completed, :kind, :position, :resource_url, :resource_title)
   end
 end
