04f2eccf6196cba18431d537b9ac099ed02f67e4
Li Ouyang <li.ouyang@gmail.com>
2022-11-16 12:23:47 -0500

    believe this line was duplicating the factories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,8 @@ require 'rspec/rails'
 require 'factory_bot_rails'
 
 FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
-FactoryBot.find_definitions
+FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), '../gems/workflow/spec/factories')
+# FactoryBot.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
