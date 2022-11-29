d894627bc0fa6d1e58df63edcf66fa31aa06f9fa
Li Ouyang <li.ouyang@gmail.com>
2022-11-29 13:31:43 -0500

    use before create hook to set and increment position

--- app/models/workflow/definition/step.rb
+++ app/models/workflow/definition/step.rb
@@ -3,5 +3,17 @@ module Workflow
     belongs_to :process
     has_many :documents, as: :documentable
     has_many :instances, class_name: 'Workflow::Instance::Step', foreign_key: 'definition_id'
+
+    before_create :set_position
+
+    DEFAULT_INCREMENT = 1000
+
+    private
+
+    def set_position
+      if self.position.nil?
+        self.position = self.process.steps.order(:position).last.try(:position).to_i + DEFAULT_INCREMENT
+      end
+    end
   end
 end
--- app/models/workflow/instance/step.rb
+++ app/models/workflow/instance/step.rb
@@ -1,15 +1,16 @@
 module Workflow
   class Instance::Step < ApplicationRecord
-    DEFAULT_INCREMENT = 1000
-
     include ApplicationRecord::ExternalIdentifier
 
     belongs_to :definition, class_name: 'Workflow::Definition::Step', optional: true
     belongs_to :process, class_name: 'Workflow::Instance::Process', counter_cache: true
     has_many :documents, as: :documentable
 
+    before_create :set_position
     after_save :update_completed_counter_cache
     after_destroy :update_completed_counter_cache
+
+    DEFAULT_INCREMENT = 1000
 
     def title
       super || self.definition.try(:title)
@@ -40,6 +41,12 @@ module Workflow
     def update_completed_counter_cache
       self.process.completed_steps_count = Workflow::Instance::Step.where(completed: true, process_id: process.id).count
       self.process.save
+    end
+
+    def set_position
+      if self.position.nil?
+        self.position = self.process.steps.order(:position).last.try(:position).to_i + DEFAULT_INCREMENT
+      end
     end
   end
 end
