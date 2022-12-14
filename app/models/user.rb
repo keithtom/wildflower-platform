e1a10a36712e67c476b132c5d535cb97f7adc44d
Li Ouyang <li.ouyang@gmail.com>
2022-12-14 14:39:05 -0500

    add the ability to reset password via email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :recoverable, 
   :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   belongs_to :person, optional: true
