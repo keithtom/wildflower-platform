873d4f8ff49c75a978a5d2e400e55db5a5b7f331
Keith Tom <keith.tom@gmail.com>
2023-02-01 12:53:05 -0800

    add spec for generate_token

--- app/commands/users/generate_token.rb
+++ app/commands/users/generate_token.rb
@@ -1,19 +1,20 @@
-class Users::GenerateToken < BaseService
+class Users::GenerateToken < BaseCommand
   def initialize(user)
     @user = user
   end
 
-  def run
+  def call
     # generate single click token for user with short expiry
-    user.authentication_token = generate_unique_token
-    user.authentication_token_at = Time.now
-    user.save!
-    user.authentication_token
+    @user.authentication_token = generate_unique_token
+    @user.authentication_token_at = Time.now
+    @user.save!
+    @user.authentication_token
   end
 
   private
 
   def generate_unique_token
     token = Devise.friendly_token until User.where(authentication_token: token).empty?
+    token
   end
 end
