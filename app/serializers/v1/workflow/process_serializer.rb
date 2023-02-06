470145d789948e42002025836f6fc929147967ad
Li Ouyang <li.ouyang@gmail.com>
2023-02-06 17:16:35 -0500

    filter out

--- app/serializers/v1/workflow/process_serializer.rb
+++ app/serializers/v1/workflow/process_serializer.rb
@@ -20,7 +20,7 @@ class V1::Workflow::ProcessSerializer < ApplicationSerializer
   has_many :steps, record_type: :workflow_instance_step, id_method_name: :external_identifier,
     serializer: V1::Workflow::StepSerializer do |process, params|
       if params[:assignee_id]
-        process.steps.where(assignee_id: params[:assignee_id])
+        process.steps.where(assignee_id: params[:assignee_id], completed: false)
       else
         process.steps
       end
