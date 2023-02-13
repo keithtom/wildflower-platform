f41bc818c630e6af229e3d82254a6cfde39b2b86
Keith Tom <keith.tom@gmail.com>
2023-02-13 14:55:38 -0800

    Update phase endpoint to return "Start considering" processes
    - add db field to definition
    - import boolean from spreadsheet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_161428) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_195704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -316,6 +316,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_161428) do
     t.datetime "updated_at", null: false
     t.integer "effort"
     t.integer "position"
+    t.boolean "start_considering", default: false
   end
 
   create_table "workflow_definition_selected_processes", force: :cascade do |t|
