86ccbf9621efe6d48c4628005e635d5c29553d27
Li Ouyang <li.ouyang@gmail.com>
2023-02-15 18:49:18 -0500

    fix phase sorting order

--- app/serializers/v1/ssj/process_progress_serializer.rb
+++ app/serializers/v1/ssj/process_progress_serializer.rb
@@ -30,7 +30,7 @@ class V1::Ssj::ProcessProgressSerializer < ApplicationSerializer
     end
 
     grouped_processes.each do |phase, status_info|
-      status_info[:statuses] = status_info[:statuses].sort_by{|status| V1::Statusable::STATUS.index(status)}
+      status_info[:statuses] = status_info[:statuses].sort_by{|status| V1::Statusable::STATUS.index(status) * -1}
     end
     return grouped_processes.values
   end
