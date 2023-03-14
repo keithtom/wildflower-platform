9b880705d395e4271277815bd3ccfc84ab26e487
Li Ouyang <li.ouyang@gmail.com>
2023-03-14 13:17:49 -0400

    wip

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -3,25 +3,39 @@ module V1
     attributes :email
 
     attribute :first_name do |user|
-      unless user.person.nil?
-        user.person.first_name
+      if person = user.person
+        person.first_name
       end
     end
 
     attribute :last_name do |user|
-      unless user.person.nil?
-        user.person.last_name
+      if person = user.person
+        person.last_name
       end
     end
 
     attribute :image_url do |user|
-      unless user.person.nil?
-        user.person.image_url
+      if person = user.person
+        person.image_url
+      end
+    end
+
+    attribute :hub do |user|
+      if person = user.person
+        person.hub.name
       end
     end
 
     belongs_to :person, serializer: V1::PersonSerializer, id_method_name: :external_identifier do |user|
       user.person
+    end
+
+    attribute :ssj do |user|
+      if person = user.person
+        ssj_team = person.ssj_teams.first
+
+
+      end
     end
   end
 end
