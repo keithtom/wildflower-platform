f0273a9129b7adc3441d85f7fe3a299cd312229d
Li Ouyang <li.ouyang@gmail.com>
2023-03-21 14:06:11 -0400

    fix constantize migration file

--- db/migrate/20230313163907_add_people_to_ssj_team.rb
+++ db/migrate/20230313163907_add_people_to_ssj_team.rb
@@ -1,4 +1,4 @@
-class AddPeopleToSsjTeam < ActiveRecord::Migration[7.0]
+class AddPeopleToSSJTeam < ActiveRecord::Migration[7.0]
   def change
     add_reference :ssj_teams, :ops_guide, foreign_key: { to_table: :people }
     add_reference :ssj_teams, :regional_growth_lead, foreign_key: { to_table: :people }
