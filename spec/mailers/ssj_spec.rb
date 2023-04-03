b90d63f6f557aa22912e867225e6317f5645b7a1
Keith Tom <keith.tom@gmail.com>
2023-04-03 13:45:32 -0700

    consolidate specs

--- spec/mailers/ssj_spec.rb
+++ spec/mailers/ssj_spec.rb
@@ -1,6 +1,23 @@
 require "rails_helper"
 
 RSpec.describe SSJMailer, type: :mailer do
+  describe "invite" do
+    let(:user) { build(:user, authentication_token: Devise.friendly_token) }
+    let(:mail) { UserMailer.invite(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Welcome to the School Startup Journey!")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["platform@email.wildflowerschools.org"])
+      expect(mail.reply_to).to eq(["support@wildflowerschools.org"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("School Startup Journey")
+      expect(mail.body.encoded).to match(user.authentication_token)
+    end
+  end
+
   describe "invite_partner" do
     let(:user) { build(:user, authentication_token: Devise.friendly_token) }
     let(:inviter) { build(:user, person: person) }
