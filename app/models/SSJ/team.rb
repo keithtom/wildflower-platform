cc648963108b401a7e91568bdd7e3044bd0b7651
Keith Tom <keith.tom@gmail.com>
2023-04-13 14:03:54 -0700

    SSJ Teams: add convenience helpers
    - added more specific helper methods for teams for the common use case
    - added member scopes

--- app/models/SSJ/team.rb
+++ app/models/SSJ/team.rb
@@ -4,8 +4,18 @@ class SSJ::Team < ApplicationRecord
   belongs_to :ops_guide, class_name: 'Person', foreign_key: 'ops_guide_id', required: false
   belongs_to :regional_growth_lead, class_name: 'Person', foreign_key: 'regional_growth_lead_id', required: false
 
-  has_many :ssj_team_members, class_name: "SSJ::TeamMember", foreign_key: 'ssj_team_id'
-  has_many :people, through: :ssj_team_members do
+  has_many :team_members, class_name: "SSJ::TeamMember"
+  
+  has_many :partner_members, -> { where(role: SSJ::TeamMember::PARTNER) }, class_name: "SSJ::TeamMember", foreign_key: 'ssj_team_id'
+  has_many :partners, through: :partner_members, source: :person
+  
+  # has_one :ops_guide_member, -> { where(role: SSJ::TeamMember::OPS_GUIDE) }, class_name: "SSJ::TeamMember", foreign_key: 'ssj_team_id'
+  # has_one :ops_guide, through: :ops_guide_member, source: :person
+
+  # has_one :rgl_member, -> { where(role: SSJ::TeamMember::RGL) }, class_name: "SSJ::TeamMember", foreign_key: 'ssj_team_id'
+  # has_one :rgl, through: :rgl_member, source: :person
+
+  has_many :people, through: :team_members do
     def active
       where('ssj_team_members.status = ?', SSJ::TeamMember::ACTIVE)
     end
