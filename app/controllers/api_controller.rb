57162d17d11ec0223d34ad6426e4dd3db4c4afd5
Keith Tom <keith.tom@gmail.com>
2023-05-02 10:23:17 -0400

    Revert "add logging for user auth problem"
    This reverts commit 01ca86bceb15cd3a2047b61bec6110d351b28e72.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,6 @@
 class ApiController < ActionController::API
   before_action :authenticate_user!
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  after_action :logging
-
-  def logging
-    puts "="*100
-    puts "current user: #{current_user.external_identifier}"
-  end
-
 
   private
   def not_found
