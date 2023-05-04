7a9c9c3a25d9a4ab1f7573b8daef6a2c2c79d143
Keith Tom <keith.tom@gmail.com>
2023-05-04 14:31:26 -0400

    fix ssj mailer spec
    - rename it
    - remove double create in factories

--- spec/factories/SSJ/teams.rb
+++ spec/factories/SSJ/teams.rb
@@ -3,10 +3,5 @@ FactoryBot.define do
     association :ops_guide, factory: :person
     association :regional_growth_lead, factory: :person
     association :workflow, factory: :workflow_instance_workflow
-    after(:build) do |team|
-      SSJ::TeamMember.create!(person: create(:person), ssj_team: team, status: SSJ::TeamMember::ACTIVE, role: SSJ::TeamMember::PARTNER)
-      SSJ::TeamMember.create!(person: team.ops_guide, ssj_team: team, status: SSJ::TeamMember::ACTIVE, role: SSJ::TeamMember::OPS_GUIDE)
-      SSJ::TeamMember.create!(person: team.regional_growth_lead, ssj_team: team, status: SSJ::TeamMember::ACTIVE, role: SSJ::TeamMember::RGL)
-    end
   end
 end
