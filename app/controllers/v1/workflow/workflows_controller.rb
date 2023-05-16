d8b5d61f3d1e2cb96f96e3eaef5f563f5b421373
Li Ouyang <li.ouyang@gmail.com>
2023-05-16 11:51:17 -0400

    fix n+1 query in /v1/workflow/workflows/:id/resources endpoint

--- app/controllers/v1/workflow/workflows_controller.rb
+++ app/controllers/v1/workflow/workflows_controller.rb
@@ -15,8 +15,8 @@ class V1::Workflow::WorkflowsController < ApiController
     definition_step_ids = Workflow::Instance::Step.where(process_id: process_ids).pluck(:definition_id)
 
     ## document could either be from instance or definition
-    documents = Document.where(documentable_id: instance_step_ids, documentable_type: Workflow::Instance::Step.to_s)
-    documents += Document.where(documentable_id: definition_step_ids, documentable_type: Workflow::Definition::Step.to_s)
+    documents = Document.where(documentable_id: instance_step_ids, documentable_type: Workflow::Instance::Step.to_s).includes(documentable: [process: [:categories]])
+    documents += Document.where(documentable_id: definition_step_ids, documentable_type: Workflow::Definition::Step.to_s).includes(documentable: [process: [:categories]])
 
     render json: V1::Workflow::ResourceSerializer.new(documents)
   end
