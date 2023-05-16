5937f70dcab0d7ec80885440066c59ff0ee3d305
Li Ouyang <li.ouyang@gmail.com>
2023-05-16 15:15:10 -0400

    require interceptor in initializer file

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,3 +1,5 @@
+require 'interceptors/reroute_email_interceptor'
+
 if Rails.env.development? || Rails.env.staging?
   ActionMailer::Base.register_interceptor(Interceptors::RerouteEmailInterceptor)
 end
