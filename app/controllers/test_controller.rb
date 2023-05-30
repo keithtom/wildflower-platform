c10e48d27c647119844a7181130c596780f47181
Li Ouyang <li.ouyang@gmail.com>
2023-05-30 11:47:09 -0400

    clean test fixtures

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,17 +1,24 @@
 class TestController < ApplicationController
   def reset_fixtures
     user = User.find_by(email: 'cypress_test@test.com')
-    ## clean deletion of everything
 
     ActiveRecord::Base.transaction do
-      if person = user && user.person
+      # delete everything associated to this user
+      person = user.person
+      user.destroy!
+      if person
         person.address&.destroy!
         person.profile_image&.purge
 
         ssj_team = person.ssj_team
-        ssj_team.partner_members.each do |partner_member|
-          partner_member.person&.destroy! # person and any partners will be destroyed
-          partner_member.destroy!
+        ssj_team.ops_guide = nil
+        ssj_team.regional_growth_lead_id = nil
+        ssj_team.save!
+
+        ssj_team.team_members.each do |team_member|
+          User.where(person_id: team_member.person_id).destroy_all
+          team_member.person&.destroy!
+          team_member.destroy!
         end
         ssj_team.destroy!
       
@@ -23,23 +30,22 @@ class TestController < ApplicationController
           process.destroy!
         end
         workflow_instance.destroy!
+
+        # create clean user, person and workflow
+        user = User.create!(email: 'cypress_test@test.com', password: 'password')
+        person = Person.create!
+        user.person = person
+        user.save!
+        Address.create!(addressable: person) if person.address.nil?
+        
+        ops_guide = FactoryBot.create(:person, role_list: "ops_guide")
+        workflow_definition = Workflow::Definition::Workflow.find_by(name: "Basic Workflow")
+        workflow_instance = SSJ::Initialize.run(workflow_definition)
+        ssj_team = SSJ::Team.create!(workflow: workflow_instance, ops_guide_id: ops_guide.id)
+
+        SSJ::TeamMember.create(person: ops_guide, ssj_team: ssj_team, role: SSJ::TeamMember::OPS_GUIDE, status: SSJ::TeamMember::ACTIVE)
+        SSJ::TeamMember.create!(person_id: person.id, ssj_team_id: ssj_team.id, role: SSJ::TeamMember::PARTNER, status: SSJ::TeamMember::ACTIVE)
       end
-
-
-      # create clean person and team member
-      user = User.create!(email: 'cypress_test@test.com', password: 'password') if user.nil?
-      person = Person.create!
-      user.person = person
-      user.save!
-      Address.create!(addressable: person) if person.address.nil?
-      
-      ops_guide = FactoryBot.create(:person, role_list: "ops_guide")
-      workflow_definition = Workflow::Definition::Workflow.find_by(name: "Basic Workflow")
-      workflow_instance = SSJ::Initialize.run(workflow_definition)
-      ssj_team = SSJ::Team.create!(workflow: workflow_instance, ops_guide_id: ops_guide.id)
-
-      SSJ::TeamMember.create(person: ops_guide, ssj_team: ssj_team, role: SSJ::TeamMember::OPS_GUIDE, status: SSJ::TeamMember::ACTIVE)
-      SSJ::TeamMember.create!(person_id: person.id, ssj_team_id: ssj_team.id, role: SSJ::TeamMember::PARTNER, status: SSJ::TeamMember::ACTIVE)
     end
   end
 end
