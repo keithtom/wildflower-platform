c7456474f69a40e7092e14d39aa34cd15dfa8881
Li Ouyang <li.ouyang@gmail.com>
2023-06-14 15:44:39 -0400

    send ops guide invite email

--- app/services/SSJ/invite_user.rb
+++ app/services/SSJ/invite_user.rb
@@ -3,6 +3,8 @@ class SSJ::InviteUser < BaseService
     @user = user
     @ops_guide_email = ops_guide_email
     @workflow_instance = nil
+    @ops_guide = nil
+    @team = nil
   end
 
   def run
@@ -10,6 +12,7 @@ class SSJ::InviteUser < BaseService
     create_workflow_instance
     create_ssj_team if @user.person.ssj_team.nil?
     Users::SendInviteEmail.call(@user)
+    Users::SendOpsGuideInviteEmail.call(@ops_guide, @team)
   end
 
   def create_person
@@ -18,8 +21,8 @@ class SSJ::InviteUser < BaseService
   end
 
   def create_ssj_team
-    ops_guide = User.find_by!(email: @ops_guide_email)
-    team = SSJ::Team.create!(workflow: @workflow_instance, ops_guide_id: ops_guide.person.id)
+    @ops_guide = User.find_by!(email: @ops_guide_email)
+    @team = SSJ::Team.create!(workflow: @workflow_instance, ops_guide_id: ops_guide.person.id)
     SSJ::TeamMember.create!(person: @user.reload.person, ssj_team: team, role: SSJ::TeamMember::PARTNER, status: SSJ::TeamMember::ACTIVE)
     SSJ::TeamMember.create!(person: ops_guide.person, ssj_team: team, role: SSJ::TeamMember::OPS_GUIDE, status: SSJ::TeamMember::ACTIVE)
   end
