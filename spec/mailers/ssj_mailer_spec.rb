5238f4cabf3d9a5c774574911c5ebb0fcebaec17
Li Ouyang <li.ouyang@gmail.com>
2023-06-14 14:59:57 -0400

    create mailer with spec

--- spec/mailers/ssj_mailer_spec.rb
+++ spec/mailers/ssj_mailer_spec.rb
@@ -74,4 +74,26 @@ RSpec.describe SSJMailer, type: :mailer do
       expect(mail.body.encoded).to match(user.authentication_token)
     end
   end
+
+  describe 'invite ops guide to new dashboard' do
+    let(:user) { build(:user, authentication_token: Devise.friendly_token) }
+    let(:team) { create(:ssj_team) }
+    let(:mail) { SSJMailer.invite_ops_guide(user, team) }
+
+    before do
+      SSJ::TeamMember.create(person: build(:person), ssj_team: team, status: SSJ::TeamMember::ACTIVE, role: SSJ::TeamMember::PARTNER)
+      SSJ::TeamMember.create(person: build(:person), ssj_team: team, status: SSJ::TeamMember::ACTIVE, role: SSJ::TeamMember::PARTNER)
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('SSJ Dashboard: You have a new team!')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["platform@email.wildflowerschools.org"])
+    end
+
+    it 'renders the dashboard url' do
+      expect(mail.body.encoded).to match(user.authentication_token)
+      expect(mail.body.encoded).to match("ssj")
+    end
+  end
 end
