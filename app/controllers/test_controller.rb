2eeeb59aa037e2d3498c2d00e3d1a26f28da782d
Li Ouyang <li.ouyang@gmail.com>
2023-12-13 09:56:13 -0500

    add roles to test fixtures and error logging

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,14 +1,14 @@
 class TestController < ApplicationController
   def reset_fixtures
+    destroy_test_records
     ActiveRecord::Base.transaction do
-      destroy_test_records
       create_test_user_with_ssj(params[:email])
     end
   end
   
   def reset_partner_fixtures
+    destroy_test_records
     ActiveRecord::Base.transaction do
-      destroy_test_records
       ssj_team = nil
       params[:emails].each_with_index do |email, index|
         user = create_test_user_with_ssj(email, ssj_team)
@@ -20,9 +20,11 @@ class TestController < ApplicationController
   end
   
   def reset_network_fixtures
+    destroy_test_records
     ActiveRecord::Base.transaction do
-      destroy_test_records
       user = create_test_user(params[:email], params[:is_onboarded])
+      user.person.role_list.add(Person::TL)
+      user.person.save
       create_school(user.person)
     end
   end
@@ -50,50 +52,56 @@ class TestController < ApplicationController
 
   def destroy_test_records
     User.where("lower(email) like ? OR lower(email) like ?", "cypress_test%", "newemail%").where("created_at < ?", 1.day.ago).each do |user|
-      person = user&.person
-      user&.destroy! 
-      if person
-        person.address&.destroy!
-        person.profile_image&.purge
-        person.school_relationships.each do |school_relationship|
-          school_relationship.destroy!
-        end
-        person.schools.each do |school|
-          school.destroy!
-        end
-
-        if ssj_team = person.ssj_team
-          ssj_team.ops_guide_id = nil
-          ssj_team.regional_growth_lead_id = nil
-          ssj_team.save!
-
-          ssj_team.team_members.each do |team_member|
-            unless team_member.person.email.include?("wildflower")
-              User.where(person_id: team_member.person_id).destroy_all
-              team_member.person&.destroy!
-            end
-            team_member.destroy!
+      begin
+        person = user&.person
+        user&.destroy! 
+        if person
+          person.address&.destroy!
+          person.profile_image&.purge
+          person.school_relationships.each do |school_relationship|
+            school_relationship.destroy!
           end
-          ssj_team.destroy!
-      
-          workflow_instance = ssj_team.workflow
-          workflow_instance.processes.each do |process|
-            process.steps.each do |step|
-              step.assignments.each do |assignment|
-                assignment.destroy!
+          person.schools.each do |school|
+            school.destroy!
+          end
+
+          if ssj_team = person.ssj_team
+            ssj_team.ops_guide_id = nil
+            ssj_team.regional_growth_lead_id = nil
+            ssj_team.save!
+
+            ssj_team.team_members.each do |team_member|
+              unless team_member.person.email.include?("wildflower")
+                User.where(person_id: team_member.person_id).destroy_all
+                team_member.person&.destroy!
               end
-              step.destroy!
+              team_member.destroy!
             end
-            process.destroy!
+            ssj_team.destroy!
+        
+            workflow_instance = ssj_team.workflow
+            workflow_instance.processes.each do |process|
+              process.steps.each do |step|
+                step.assignments.each do |assignment|
+                  assignment.destroy!
+                end
+                step.destroy!
+              end
+              process.destroy!
+            end
+            workflow_instance.destroy!
           end
-          workflow_instance.destroy!
         end
+      rescue => e
+        Rails.logger.error("Unable to detroy user record and associated records for id #{user.id}: #{e.message}")
       end
     end
   end
 
   def create_test_user_with_ssj(email, ssj_team = nil, is_onboarded = false)
     user = create_test_user(email, is_onboarded)
+    user.person.role_list.add(Person::ETL)
+    user.person.save!
     
     if ssj_team.nil?
       ops_guide = FactoryBot.create(:person, role_list: "ops_guide")
