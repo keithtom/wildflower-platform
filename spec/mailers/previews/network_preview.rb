b20a10e2f4cda021a15784da2a9f2d856caf0a59
Li Ouyang <li.ouyang@gmail.com>
2024-01-09 11:05:39 -0500

    first draft of mailer

--- spec/mailers/previews/network_preview.rb
+++ spec/mailers/previews/network_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/network
 class NetworkPreview < ActionMailer::Preview
-
+  def remind_login
+    NetworkMailer.remind_login(User.first)
+  end
 end
