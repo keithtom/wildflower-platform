3ca31482af6f3e64b18798a3636f01276a5894b6
Li Ouyang <li.ouyang@gmail.com>
2024-03-26 11:08:44 -0400

    fix specs to have the included

--- app/serializers/v1/workflow/definition/workflow_serializer.rb
+++ app/serializers/v1/workflow/definition/workflow_serializer.rb
@@ -2,4 +2,8 @@ class V1::Workflow::Definition::WorkflowSerializer < ApplicationSerializer
   set_id :id
 
   attributes :name, :description, :version
+
+  has_many :processes, serializer: V1::Workflow::Definition::BasicProcessSerializer do |workflow|
+    workflow.processes.order(:position)
+  end
 end
