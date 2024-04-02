d73fa60a4e8a970661f9509fcf569ae5ba74ff75
Li Ouyang <li.ouyang@gmail.com>
2024-04-02 16:13:12 -0400

    write service to propogate step instaneous changes

--- app/services/workflow/definition/step/propogate_change.rb
+++ app/services/workflow/definition/step/propogate_change.rb
@@ -4,36 +4,42 @@ module Workflow
       class PropogateChange
         VALID_ATTR_CHANGES = [
           :title, :description, :position, :completion_type, :min_worktime, :max_worktime, :decision_question, 
-          decision_options_attributes: [:id, :description], 
-          documents_attributes: [:id, :title, :link]
         ]
 
         def initialize(step_definition, param_changes)
           @step_definition = step_definition
-          @param_changes = param_changes.to_hash
+          @param_changes = param_changes.to_hash.with_indifferent_access
         end
       
         def run
+          scrub_param_changes
           validate_param_changes
-          # validate that the param changes are only the ones we allow
-          # grab all the step_instances
-          # see what the 
-          # maybe update from the param changes?
+          update_instances
         end
       
+        private 
+        def scrub_param_changes
+          @param_changes.delete(:documents_attributes)
+          @param_changes.delete(:decision_option_attributes)
+        end
+
         def validate_param_changes
           action_on_unpermitted_parameters = ActionController::Parameters.action_on_unpermitted_parameters
           ActionController::Parameters.action_on_unpermitted_parameters = :raise
 
           begin
             ActionController::Parameters.new(@param_changes).permit(VALID_ATTR_CHANGES)
-          rescue ActionController::UnpermittedParameters 
-            raise StandardError.new("Attribute(s) cannot be an instantaneously changed")
+          rescue ActionController::UnpermittedParameters => e
+            raise StandardError.new("Attribute(s) cannot be an instantaneously changed: #{e.params.join(", ")}")
 
             ActionController::Parameters.action_on_unpermitted_parameters = action_on_unpermitted_parameters
           end
 
           ActionController::Parameters.action_on_unpermitted_parameters = action_on_unpermitted_parameters
+        end
+      
+        def update_instances
+          @step_definition.instances.update_all(@param_changes)
         end
       end
     end
--- spec/services/workflow/definition/step/propogate_change_spec.rb
+++ spec/services/workflow/definition/step/propogate_change_spec.rb
@@ -2,29 +2,37 @@ require "rails_helper"
 
 describe Workflow::Definition::Step::PropogateChange do
   describe "#validate_param_changes" do
+    let(:step) { create(:workflow_definition_step) }
     let(:param_changes) { 
       {
         "title"=>"Updated Step",                                                                                                            
         "description"=>"This is an updated step",                                                                                           
-        "kind"=>"decision",                                                                                                                 
         "position"=>2,                                                     
         "completion_type"=>"one_per_group",                                
         "decision_question"=>"Are you sure?",                              
-        "documents_attributes"=>[{"title"=>"basic resource", "link"=>"www.example.com"}]
+        "documents_attributes"=>[{"title"=>"basic resource", "link"=>"www.example.com"}] # adding a document, what about updating one?
       }  
     }
 
-    let(:valid_attr_changes) { [:attr1, :attr2] }
-
     it "should permit valid attribute changes" do
-      subject = described_class.new(param_changes)
-      expect { subject.validate_param_changes }.not_to raise_error
+      subject = described_class.new(step, param_changes)
+      expect { subject.run }.not_to raise_error
     end
 
     it "should raise an error for unpermitted attribute changes" do
-      param_changes[:attr3] = "value3"
-      subject = described_class.new(param_changes)
-      expect { subject.validate_param_changes }.to raise_error(StandardError, "Attribute(s) cannot be an instantaneously changed")
+      param_changes[:kind] = "decision"
+      subject = described_class.new(step, param_changes)
+      expect { subject.run }.to raise_error(StandardError, "Attribute(s) cannot be an instantaneously changed: kind")
+    end
+
+    it "should raise an error for unpermitted attribute changes" do
+      step_instance = create(:workflow_instance_step, definition: step)
+      expect(step_instance.title).to_not eq("Updated Step")
+
+      subject = described_class.new(step, param_changes)
+      subject.run
+    
+      expect(step_instance.reload.title).to eq("Updated Step")
     end
   end
 end
