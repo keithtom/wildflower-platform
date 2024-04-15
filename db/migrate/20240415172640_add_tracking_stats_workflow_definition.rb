c27ea3d85bac0c08a3f22dc78dfcad2225dbcef1
Li Ouyang <li.ouyang@gmail.com>
2024-04-15 13:32:07 -0400

    rollout stats added

---
+++ db/migrate/20240415172640_add_tracking_stats_workflow_definition.rb
@@ -0,0 +1,6 @@
+class AddTrackingStatsWorkflowDefinition < ActiveRecord::Migration[7.0]
+  def change
+    add_column :workflow_definition_workflows, :rollout_started_at, :datetime
+    add_column :workflow_definition_workflows, :rollout_completed_at, :datetime
+  end
+end
