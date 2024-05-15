e43be5cddbd0a989a77f94444ad3b23a2926af28
Li Ouyang <li.ouyang@gmail.com>
2024-05-15 11:12:41 -0400

    wip

--- app/serializers/v1/ssj/team_serializer.rb
+++ app/serializers/v1/ssj/team_serializer.rb
@@ -1,5 +1,5 @@
 class V1::SSJ::TeamSerializer < ApplicationSerializer
-  attributes :expected_start_date, :temp_name, :temp_location
+  attributes :expected_start_date, :temp_name
   
   attribute :workflow_id do |team|
     team.workflow&.external_identifier
@@ -13,11 +13,19 @@ class V1::SSJ::TeamSerializer < ApplicationSerializer
     team.partners.count > 1
   end
   
+  attribute :temp_location do |team, params|
+    if params[:team_id]
+      team.temp_location
+    end
+  end
+  
   attribute :invited_partner do |team|
     team.partner_members.invited.count > 0
   end
   
-  has_many :partners, serializer: V1::PersonSerializer ,id_method_name: :external_identifier do |team|
-    team.partners.active
+  has_many :partners, serializer: V1::PersonSerializer ,id_method_name: :external_identifier do |team, params|
+    if params[:team_id]
+      team.partners.active
+    end
   end
 end
