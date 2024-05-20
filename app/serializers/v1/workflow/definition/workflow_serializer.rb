efa821636fe470f275ffbdfcbb763ac815924773
Li Ouyang <li.ouyang@gmail.com>
2024-05-20 12:35:28 -0400

    go back to compact style

--- app/serializers/v1/workflow/definition/workflow_serializer.rb
+++ app/serializers/v1/workflow/definition/workflow_serializer.rb
@@ -1,33 +1,27 @@
 # frozen_string_literal: true
 
-module V1
-  module Workflow
-    module Definition
-      class WorkflowSerializer < ApplicationSerializer
-        set_id :id
+class V1::Workflow::Definition::WorkflowSerializer < ApplicationSerializer
+  set_id :id
 
-        attributes :name, :description, :version, :created_at, :rollout_started_at, :rollout_completed_at, :previous_version_id
+  attributes :name, :description, :version, :created_at, :rollout_started_at, :rollout_completed_at, :previous_version_id
 
-        attribute :num_of_versions do |workflow|
-          Workflow::Definition::Workflow.where(name: workflow.name).count
-        end
+  attribute :num_of_versions do |workflow|
+    Workflow::Definition::Workflow.where(name: workflow.name).count
+  end
 
-        attribute :num_of_instances do |workflow|
-          workflow.instances.count
-        end
+  attribute :num_of_instances do |workflow|
+    workflow.instances.count
+  end
 
-        attribute :published, &:published?
+  attribute :published, &:published?
 
-        attribute :rollout_in_progress do |workflow|
-          !workflow.rollout_started_at.nil? && workflow.rollout_completed_at.nil?
-        end
+  attribute :rollout_in_progress do |workflow|
+    !workflow.rollout_started_at.nil? && workflow.rollout_completed_at.nil?
+  end
 
-        has_many :processes, serializer: V1::Workflow::Definition::BasicProcessSerializer do |workflow, params|
-          if params[:workflow_id]
-            workflow.displayed_processes.includes(:taggings, :categories).order('workflow_definition_selected_processes.position')
-          end
-        end
-      end
+  has_many :processes, serializer: V1::Workflow::Definition::BasicProcessSerializer do |workflow, params|
+    if params[:workflow_id]
+      workflow.displayed_processes.includes(:taggings, :categories).order('workflow_definition_selected_processes.position')
     end
   end
 end
