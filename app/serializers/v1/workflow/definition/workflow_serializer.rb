1320ae5469276fea08b9e82e37d5154749d877e0
Li Ouyang <li.ouyang@gmail.com>
2024-05-20 11:58:21 -0400

    if a step change does not involve position, it should still go through

--- app/serializers/v1/workflow/definition/workflow_serializer.rb
+++ app/serializers/v1/workflow/definition/workflow_serializer.rb
@@ -1,27 +1,33 @@
-class V1::Workflow::Definition::WorkflowSerializer < ApplicationSerializer
-  set_id :id
+# frozen_string_literal: true
 
-  attributes :name, :description, :version, :created_at, :rollout_started_at, :rollout_completed_at, :previous_version_id
+module V1
+  module Workflow
+    module Definition
+      class WorkflowSerializer < ApplicationSerializer
+        set_id :id
 
-  attribute :num_of_versions do |workflow|
-    Workflow::Definition::Workflow.where(name: workflow.name).count
-  end
+        attributes :name, :description, :version, :created_at, :rollout_started_at, :rollout_completed_at, :previous_version_id
 
-  attribute :num_of_instances do |workflow|
-    workflow.instances.count
-  end
+        attribute :num_of_versions do |workflow|
+          Workflow::Definition::Workflow.where(name: workflow.name).count
+        end
 
-  attribute :published do |workflow|
-    workflow.published?
-  end
-  
-  attribute :rollout_in_progress do |workflow|
-    !workflow.rollout_started_at.nil? && workflow.rollout_completed_at.nil?
-  end
-  
-  has_many :processes, serializer: V1::Workflow::Definition::BasicProcessSerializer do |workflow, params|
-    if params[:workflow_id]
-      workflow.displayed_processes.includes(:taggings, :categories).order('workflow_definition_selected_processes.position')
+        attribute :num_of_instances do |workflow|
+          workflow.instances.count
+        end
+
+        attribute :published, &:published?
+
+        attribute :rollout_in_progress do |workflow|
+          !workflow.rollout_started_at.nil? && workflow.rollout_completed_at.nil?
+        end
+
+        has_many :processes, serializer: V1::Workflow::Definition::BasicProcessSerializer do |workflow, params|
+          if params[:workflow_id]
+            workflow.displayed_processes.includes(:taggings, :categories).order('workflow_definition_selected_processes.position')
+          end
+        end
+      end
     end
   end
 end
