3831aca7ad3f772d0969e9fd7c86a537ee37a657
Li Ouyang <li.ouyang@gmail.com>
2024-06-15 15:15:51 -0400

    remove logging, especially with the wrong varibale name'

--- app/services/workflow/instance/dependency/create.rb
+++ app/services/workflow/instance/dependency/create.rb
@@ -33,9 +33,6 @@ module Workflow
           end
 
           if @prerequisite_workable.nil?
-            Rails.logger.info("wf_instance_id: #{@wf_instance.id}")
-            Rails.logger.info("dependecy definition_id: #{@dependency_definition.id}")
-            Rails.logger.info("prerequisite_process_ids: #{prerequisite_process_ids}")
             raise CreateError.new("prerequisite workable not found for dependency def #{@dependency_definition.id} and workflow instance id #{@wf_instance.id}")
           end
         end
