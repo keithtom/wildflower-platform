36d46f23815e997a9947a6ee5412908dd6426dd5
Li Ouyang <li.ouyang@gmail.com>
2024-06-20 09:54:31 -0400

    noop good_job initializer

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,5 +1,5 @@
-GoodJob.on_thread_error = -> (exception) do 
-  Rails.logger.error("Error executing background job")
-  Highlight::H.instance.record_exception(exception)
-  SlackClient.chat_postMessage(channel: '#circle-platform', text: exception.message, as_user: true)
-end
+# GoodJob.on_thread_error = -> (exception) do 
+#   Rails.logger.error("Error executing background job")
+#   Highlight::H.instance.record_exception(exception)
+#   SlackClient.chat_postMessage(channel: '#circle-platform', text: exception.message, as_user: true)
+# end
