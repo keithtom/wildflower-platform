f5d927e7a32491d854369a169abfe1c8497ad770
Li Ouyang <li.ouyang@gmail.com>
2024-07-23 12:10:13 -0400

    only a person that is part of the ssj team or school can be assigned to a step from a workflow

--- app/services/workflow/instance/step/assign_person.rb
+++ app/services/workflow/instance/step/assign_person.rb
@@ -8,11 +8,23 @@ module Workflow
       end
 
       def run
+        validate_step_and_person
         assign_person
         update_process_completion_status
       end
 
       private
+
+      def validate_step_and_person
+        teams = @person.schools
+        teams += [@person.ssj_team] if @person.ssj_team
+
+        teams.each do |team|
+          team.workflow_id == @process.workflow_id
+          return true
+        end
+        raise AssignPersonError.new("Person email #{@person.email} cannot be assigned to this step because not part of the team/school")
+      end
 
       def assign_person
         # NOTE: if the step worktype was "only 1 assigner", we'd unassign first.
@@ -24,6 +36,8 @@ module Workflow
       def update_process_completion_status
         @process.started!
       end
+    end
+    class AssignPersonError < StandardError
     end
   end
 end
