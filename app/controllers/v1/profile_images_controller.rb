1ba8ae6459e64c16a1767ef6b7faf5b4094fc0eb
Li Ouyang <li.ouyang@gmail.com>
2024-10-28 11:47:55 -0400

    FE has to make a separate call

--- app/controllers/v1/profile_images_controller.rb
+++ app/controllers/v1/profile_images_controller.rb
@@ -14,9 +14,7 @@ class V1::ProfileImagesController < ApiController
 
       # Generate the URL for the processed variant
       url = variant_processed.url
-      url = url.sub("#{ENV.fetch('S3_BUCKET', 'ssj-local')}.s3.amazonaws.com", ENV.fetch('ASSET_HOST')) if ENV.fetch(
-        'ASSET_HOST', false
-      )
+      url = ImageHelper.cdn_url(url)
       redirect_to(url, allow_other_host: true)
     else
       render json: { error: 'Profile image not found' }, status: :not_found
--- app/serializers/v1/person_serializer.rb
+++ app/serializers/v1/person_serializer.rb
@@ -56,8 +56,8 @@ module V1
 
     attribute :image_url do |person|
       if person.profile_image.attached?
-        Rails.application.routes.url_helpers.v1_person_profile_image_url(person_id: person.external_identifier)
-        # Rails.application.routes.url_helpers.rails_blob_url(person.profile_image)
+        # Rails.application.routes.url_helpers.v1_person_profile_image_url(person_id: person.external_identifier)
+        ImageHelper.cdn_url(Rails.application.routes.url_helpers.rails_blob_url(person.profile_image))
       elsif person.image_url.present?
         person.image_url
       end
