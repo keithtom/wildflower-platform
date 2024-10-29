7cba20439d0f49f2552b43411ebb08c01e6e8951
Li Ouyang <li.ouyang@gmail.com>
2024-10-29 11:04:28 -0400

    revert changes to staging

--- app/serializers/v1/person_basic_serializer.rb
+++ app/serializers/v1/person_basic_serializer.rb
@@ -10,7 +10,10 @@ module V1
 
     attribute :image_url do |person|
       if person.profile_image.attached?
-        ImageHelper.cdn_url(Rails.application.routes.url_helpers.rails_blob_url(person.profile_image))
+        signed_id = person.signed_id(expires_in: 1.hour)
+        Rails.application.routes.url_helpers.v1_person_profile_image_url(
+          person_id: person.external_identifier, signed_id:
+        )
       elsif person.image_url.present?
         person.image_url
       end
