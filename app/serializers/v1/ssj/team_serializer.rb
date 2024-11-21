8293f58ad25804e5fc23e2ad802f5079397b8d56
Li Ouyang <li.ouyang@gmail.com>
2024-11-21 12:44:20 -0500

    add ops guide and rgl to teams api

--- app/serializers/v1/ssj/team_serializer.rb
+++ app/serializers/v1/ssj/team_serializer.rb
@@ -1,6 +1,6 @@
 class V1::SSJ::TeamSerializer < ApplicationSerializer
   attributes :expected_start_date, :temp_name, :temp_location
-  
+
   attribute :workflow_id do |team|
     team.workflow&.external_identifier
   end
@@ -12,12 +12,21 @@ class V1::SSJ::TeamSerializer < ApplicationSerializer
   attribute :has_partner do |team|
     team.partners.count > 1
   end
-  
+
   attribute :invited_partner do |team|
     team.partner_members.invited.count > 0
   end
-  
-  has_many :partners, serializer: V1::PersonSerializer ,id_method_name: :external_identifier do |team|
+
+  has_many :partners, serializer: V1::PersonSerializer, id_method_name: :external_identifier do |team|
     team.partners.active
+  end
+
+  belongs_to :ops_guide, serializer: V1::PersonBasicSerializer, id_method_name: :external_identifier do |team|
+    team.ops_guide
+  end
+
+  belongs_to :regional_growth_lead, serializer: V1::PersonBasicSerializer,
+                                    id_method_name: :external_identifier do |team|
+    team.regional_growth_lead
   end
 end
