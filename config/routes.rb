c04e51b6de7043e523be39f5efd9f18e0f8035ea
Li Ouyang <li.ouyang@gmail.com>
2024-12-04 15:32:49 -0500

    change to put

--- config/routes.rb
+++ config/routes.rb
@@ -128,7 +128,7 @@ Rails.application.routes.draw do
   put 'reset_network_fixtures', to: 'test#reset_network_fixtures' if ENV['CYPRESS_ENABLED'] == 'true'
   put 'reset_open_school_fixtures', to: 'test#reset_open_school_fixtures' if ENV['CYPRESS_ENABLED'] == 'true'
   get 'reset_rollout_workflow_fixture', to: 'test#reset_rollout_workflow_fixture' if ENV['CYPRESS_ENABLED'] == 'true'
-  get 'reset_test_school', to: 'test#reset_test_school' if ENV['CYPRESS_ENABLED'] == 'true'
+  put 'reset_test_school', to: 'test#reset_test_school' if ENV['CYPRESS_ENABLED'] == 'true'
   get 'invite_email_link', to: 'test#invite_email_link' if ENV['CYPRESS_ENABLED'] == 'true'
   get 'network_invite_email_link', to: 'test#network_invite_email_link' if ENV['CYPRESS_ENABLED'] == 'true'
 end
