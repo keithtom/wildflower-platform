9daa5064f7799cddf76f7ac0f2618987246b5751
Li Ouyang <li.ouyang@gmail.com>
2025-01-22 14:48:30 -0500

    adds tests

--- spec/requests/v1/schools_spec.rb
+++ spec/requests/v1/schools_spec.rb
@@ -3,65 +3,82 @@ require 'rails_helper'
 describe 'API V1 School', type: :request do
   let(:user) { create(:user) }
   let(:school) { create(:school) }
-  let(:person) { create(:person)}
+  let(:person) { create(:person) }
   let(:address) { create(:address) }
 
   before do
-    create(:school_relationship, person: person, school: school)
+    sr = create(:school_relationship, person:, school:)
+    sr.role_list.add(Person::TL)
 
     create(:school)
     sign_in(user)
   end
 
-  describe "GET /v1/schools" do
-    it "succeeds" do
-      get "/v1/schools", headers: {'ACCEPT' => 'application/json' }
+  describe 'GET /v1/schools' do
+    it 'succeeds' do
+      get '/v1/schools', headers: { 'ACCEPT' => 'application/json' }
       expect(response).to have_http_status(:success)
+    end
+
+    describe 'with person_id query parameter' do
+      it 'succeeds' do
+        get "/v1/schools?person_id=#{person.id}", headers: { 'ACCEPT' => 'application/json' }
+        expect(response).to have_http_status(:success)
+        expect(json_response['data'].first['id']).to be(school.external_identifier)
+      end
+    end
+
+    describe 'with person_id query parameter' do
+      it 'succeeds' do
+        get "/v1/schools?person_id=#{person.id}&role=Ops%20Guide", headers: { 'ACCEPT' => 'application/json' }
+        expect(response).to have_http_status(:success)
+        expect(json_response['data']).to be_empty
+      end
     end
   end
 
-  describe "GET /v1/schools/1" do
-    it "succeeds" do
+  describe 'GET /v1/schools/1' do
+    it 'succeeds' do
       Bullet.enable = false ## failing on Github Actions CI, but not locally, no idea why.
-      get "/v1/schools/#{school.external_identifier}", headers: { 'ACCEPT' => 'application/json'}
+      get "/v1/schools/#{school.external_identifier}", headers: { 'ACCEPT' => 'application/json' }
       expect(response).to have_http_status(:success)
-      expect(json_response["included"]).to include(have_type(:person).and have_attribute(:email))
+      expect(json_response['included']).to include(have_type(:person).and(have_attribute(:email)))
       Bullet.enable = true
     end
   end
 
-  describe "PUT /v1/schools/1" do
+  describe 'PUT /v1/schools/1' do
     let(:person1) { create(:person) }
     let(:person2) { create(:person) }
     let(:person3) { create(:person) }
 
-    it "succeeds" do
+    it 'succeeds' do
       current_school_id = school.address.id
-      put "/v1/schools/#{school.external_identifier}", 
-      params: { school: { 
-        about: 'new about',
-        school_relationships_attributes: [
-          { person_id: person1.id },
-          { person_id: person2.id },
-          { person_id: person3.id },
-      ],
-        address_attributes: {
-          city: 'new city',
-          state: 'new state',
-        },
-        opened_on: '2018-01-01',
-        ages_served_list: ['elementary', 'middle'],
-        governance_type: 'charter',
-        max_enrollment: 100
-      } }, 
-      headers: { 'ACCEPT' => 'application/json'}
+      put "/v1/schools/#{school.external_identifier}",
+          params: { school: {
+            about: 'new about',
+            school_relationships_attributes: [
+              { person_id: person1.id },
+              { person_id: person2.id },
+              { person_id: person3.id }
+            ],
+            address_attributes: {
+              city: 'new city',
+              state: 'new state'
+            },
+            opened_on: '2018-01-01',
+            ages_served_list: %w[elementary middle],
+            governance_type: 'charter',
+            max_enrollment: 100
+          } },
+          headers: { 'ACCEPT' => 'application/json' }
       expect(response).to have_http_status(:success)
 
       school.reload
       expect(school.address.id).to eq(current_school_id)
       expect(school.address.city).to eq('new city')
       expect(school.people).to include(person1, person2, person3)
-      expect(school.ages_served_list).to eq(['elementary', 'middle'])
+      expect(school.ages_served_list).to eq(%w[elementary middle])
     end
   end
 end
