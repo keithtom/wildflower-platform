8d0ab056b958375d0e8cf653414959217b260b3d
Li Ouyang <li.ouyang@gmail.com>
2025-02-05 12:38:49 -0500

    remove from this PR

--- app/serializers/v1/workflow/workflow_serializer.rb
+++ app/serializers/v1/workflow/workflow_serializer.rb
@@ -1,7 +1,7 @@
 class V1::Workflow::WorkflowSerializer < ApplicationSerializer
   singleton_class.include Rails.application.routes.url_helpers
 
-  attributes :name, :description, :version, :recurring
+  attributes :name, :description, :version
 
   # has_many :processes, serializer: V1::Workflow::ProcessSerializer,
   #   id_method_name: :external_identifier do |workflow|
