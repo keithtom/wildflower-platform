19f553179962bb52a48d07ac73566d3a582eac78
Li Ouyang <li.ouyang@gmail.com>
2025-02-11 11:38:27 -0500

    make sure school relationships created for ops guides and rgl's when school created

--- app/services/SSJ/invite_school.rb
+++ app/services/SSJ/invite_school.rb
@@ -83,6 +83,14 @@ class SSJ::InviteSchool < BaseService
       sr.role_list.add(Person::ETL)
       sr.save!
     end
+
+    sr = SchoolRelationship.create!(school_id: @school.id, person_id: @ops_guide.id)
+    sr.role_list.add(Person::OPS_GUIDE)
+    sr.save!
+
+    sr = SchoolRelationship.create!(school_id: @school.id, person_id: @regional_growth_leader.id)
+    sr.role_list.add(Person::RGL)
+    sr.save!
   end
 
   def send_emails
