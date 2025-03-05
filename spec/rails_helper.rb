abaa3127d0daac4c841510f5dfd62899e93add68
Li Ouyang <li.ouyang@gmail.com>
2025-03-05 14:14:24 -0500

    fix rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'factory_bot_rails'
-require 'database_cleaner'
+require 'database_cleaner/active_record'
 
 FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
 # FactoryBot.find_definitions
